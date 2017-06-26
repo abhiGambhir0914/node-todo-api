55db1e83f9db338858072e76d0bf252ccc8c2e94
System Administrator <root@Neelams-MacBook-Pro.local>
2017-06-27 01:43:17 +0530

    Missed the id keyword in function isValid

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.get('/todos',(req,res)=>{
 app.get('/todos/:id',(req,res)=>{
   var id=req.params.id;
 
-  if(!ObjectID.isValid){
+  if(!ObjectID.isValid(id)){
     return res.status(404).send();
   }
 
