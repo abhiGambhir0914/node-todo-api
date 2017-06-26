9d360886ec502c6508121773ef434e0c83487376
System Administrator <root@Neelams-MacBook-Pro.local>
2017-06-27 01:32:14 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser =require('body-parser');
+const {ObjectID}= require('mongodb');
+
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -28,6 +30,27 @@ app.get('/todos',(req,res)=>{
       res.status(500).send(err);
   });
 });
+
+// GET /todos/1234
+
+app.get('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+
+  if(!ObjectID.isValid){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  },(err)=>{
+    res.status(400).send();
+  });
+
+});
+
 
 app.listen(3000,()=>{
   console.log('Server is set up at port 3000');
