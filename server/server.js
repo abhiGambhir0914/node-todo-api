5b1f99a36ca6fb7bca1e801b55eccc743d9ce5b1
System Administrator <root@Neelams-MacBook-Pro.local>
2017-06-28 22:07:39 +0530

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser =require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser =require('body-parser');
 const {ObjectID}= require('mongodb');
 
 
@@ -71,6 +72,39 @@ app.delete('/todos/:id',(req,res)=>{
     res.status(400).send();
   });
 });
+
+
+app.patch('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+  var body=_.pick(req.body,['text','completed']);
+
+  if(!ObjectID.isValid(id))
+  {
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed ){
+    body.completedAt = new Date().getTime();
+  }
+  else{
+    body.completed=false;
+    body.completedAt=null;
+  }
+
+  Todo.findByIdAndUpdate(id,body,{new:true}).then((todo)=>{
+    if(!todo){
+      return res.send(404).send();
+    }
+    res.send({todo});
+  }).catch((err)=>{
+    res.status(400).send();
+  });
+
+});
+
+
+
+
 
 app.listen(port,()=>{
   console.log(`Server is set up at port ${port}`);
