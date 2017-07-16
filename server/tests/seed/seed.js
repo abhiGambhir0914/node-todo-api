c318efca6ee8ec901255a7a6e708fc3a24917390
System Administrator <root@Neelams-MacBook-Pro.local>
2017-07-16 16:51:04 +0530

    Associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -17,17 +17,24 @@ const users=[{
 },{
   _id:userTwoId,
   email:'abcd@exam.com',
-  password:'userTwoPass'
+  password:'userTwoPass',
+  tokens:[{
+    access:'auth',
+    token: jwt.sign({_id: userTwoId, access:'auth'},'123abc').toString()
+  }]
 }];
+
 
 const todos=[{
   _id:new ObjectID(),
-  text: 'First test todo'
+  text: 'First test todo',
+  _creator: userOneId
 },{
   _id:new ObjectID(),
   text: 'Second test todo',
   completed: true,
-  completedAt: 3422
+  completedAt: 3422,
+  _creator: userTwoId
 }];
 
 const populateTodos=(done)=>{
